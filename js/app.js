fca834369a0ff72537a2a436cd4c21dbe04325c2
Carl van Straalen <carlvanstraalen@gmail.com>
2017-06-10 20:49:16 +0200

    New start

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ function loadRacers(racerList) {
   racerList.forEach(function (racer) {
     config.racers[racer.id] = {
       id:         racer.id,
-      img:        racer.img,
+      table_img:  racer.table_img,
       name:       racer.name,
       kart:       racer.kart
     };
@@ -18,6 +18,8 @@ function loadRacers(racerList) {
 
 function updateHTML(table) {
   var output = "";
+  var fillInButton = "js-fill-in-time-button";
+  var positions = $('.time-table >tbody >tr').length + 1;
   table.forEach(function (racer) {
     var classList = "";
     if (racer.movement === "up") {
@@ -26,22 +28,19 @@ function updateHTML(table) {
       classList = "animated fadeInDown down";
     }
 
-    var positions = document.getElementsByTagName("table").getElementsByTagName("tbody")[0].getElementsByTagName("tr").length;
-
     output += "<tr class=\"" + classList + "\">";
     output += "<td>" + positions + "." + "</td>";
-    output += "<td>" + racer.img + "</td>";
-    output += "<td class=" + '"js-fill-in-time-button">' + racer.name + "</td>";
+    output += "<td>" + racer.table_img + "</td>";
+    output += "<td class=\"" + fillInButton + "\">" + racer.name + "</td>";
     output += "<td>" + racer.kart + "</td>";
     output += "<td>" + "0" + "</td>";
-    output += "<td>" + "00:00.000" + "</td>";
+    output += "<td>" + "0:00:00.000" + "</td>";
     output += "<td>" + "+00:00.000" + "</td>";
     output += "</tr>\n";
   });
 
-  $("table > tbody").innerHTML = output;
+  $$("table > tbody").innerHTML = output;
 }
-
 
 function bootstrap() {
   var initialTable = [];
@@ -58,7 +57,7 @@ function bootstrap() {
   return initialTable;
 }
 
-function $(expr) {
+function $$(expr) {
   if (document.querySelectorAll(expr).length === 1) {
       return document.querySelectorAll(expr)[0];
   } else {
