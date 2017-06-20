91275c38edf973532842569bb792767258f0b46e
Carl van Straalen <carlvanstraalen@gmail.com>
2017-06-21 00:49:38 +0200

    Time will be pushed to animation, but still WIP

--- js/fill-in-time.js
+++ js/fill-in-time.js
@@ -1,15 +1,31 @@
 var raceMap = $(".circuit-map");
-       lap1 = "",
-       lap2 = "",
-       lap3 = "";
 
 function enterTime() {
   $(".js-fill-in-time-button").on("click", function() {
-    prompt('Enter ' + $(this).data("racer") + "'s" + ' 1st lap time in milliseconds');
-    prompt('Enter ' + $(this).data("racer") + "'s" + ' 2nd lap time in milliseconds');
-    prompt('Enter ' + $(this).data("racer") + "'s" + ' final lap time in milliseconds');
+    var lap1 = prompt("Enter " + $(this).data("racer") + "'s" + " 1st lap time in milliseconds", "2500"),
+        lap2 = prompt("Enter " + $(this).data("racer") + "'s" + " 2nd lap time in milliseconds", "5000"),
+        lap3 = prompt("Enter " + $(this).data("racer") + "'s" + " final lap time in milliseconds", "10000");
 
-    // TODO: Img + class will turn into two variables that will find the selected racer
     raceMap.append("<img data-img=\"" + $(this).data("img") + "\" src=\"" + "img/circuits/heads/" + $(this).data("img") + ".png" + "\" class=\"" + "luigi-circuit" + "\">");
+
+    if (lap1, lap2, lap3 === null) {
+      return;
+    }
+
+    setTimeout(function() {
+      $(".luigi-circuit").css("animation-duration", lap1 + "ms");
+      console.log(lap1);
+    });
+
+    setTimeout(function() {
+      $(".luigi-circuit").css("animation-duration", lap2 +"ms");
+      console.log(lap2);
+    }, lap1);
+
+    setTimeout(function() {
+      $(".luigi-circuit").css("animation-duration", lap3 + "ms");
+      console.log(lap3);
+    }, lap2);
+
   });
 };
