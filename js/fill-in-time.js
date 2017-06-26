d58efcc67a91ff71d1cb555386002971d7c2e768
Carl van Straalen <carlvanstraalen@gmail.com>
2017-06-26 21:04:20 +0200

    Added lapCounter

--- js/fill-in-time.js
+++ js/fill-in-time.js
@@ -3,34 +3,44 @@ function enterTime() {
     var lap1 = prompt("Enter " + $(this).data("racer") + "'s" + " 1st lap time in milliseconds", "2000"),
         lap2 = prompt("Enter " + $(this).data("racer") + "'s" + " 2nd lap time in milliseconds", "2000"),
         lap3 = prompt("Enter " + $(this).data("racer") + "'s" + " final lap time in milliseconds", "2000"),
-        lapCounter = "2";
+        lapCounter = 1;
 
-    $(".circuit-map").append("<img data-img=\"" + $(this).data("img") + "\" src=\"" + "img/circuits/heads/" + $(this).data("img") + ".png" + "\" class=\"" + "luigi-circuit-lap-1" + "\">");
+    $(".circuit-map").append("<img data-img=\"" + $(this).data("img") + "\" src=\"" + "img/circuits/heads/" + $(this).data("img") + ".png" + "\" class=\"" + "js-circuit" + "\">");
 
     if (lap1, lap2, lap3 === null) {
       return;
     }
 
-    setTimeout(function() {
-      $(".luigi-circuit-lap-1").css("animation-duration", lap1 + "ms");
-      console.log("Lap 1: " + lap1);
-    });
+    doAnimate("luigi-circuit", parseInt(lap1));
+    $(".js-lap-counter").text(lapCounter);
+    console.log("Lap 1: " + lap1);
 
     setTimeout(function() {
-      $(".luigi-circuit-lap-1").removeClass("luigi-circuit-lap-1").addClass("luigi-circuit-lap-2").css("animation-duration", lap2 + "ms");
+      doAnimate("luigi-circuit", parseInt(lap2));
       console.log("Lap 2: " + lap2);
-      $(".js-lap-counter").append(lapCounter++);
-    }, lap1);
+      lapCounter++;
+      $(".js-lap-counter").text(lapCounter);
 
-    setTimeout(function() {
-      $(".luigi-circuit-lap-2").removeClass("luigi-circuit-lap-2").addClass("luigi-circuit-lap-3").css("animation-duration", lap3 + "ms");
-      console.log("Final lap: " + lap3);
-      $(".js-lap-counter").append(lapCounter++);
-    }, parseInt(lap1) + parseInt(lap2));
+      setTimeout(function() {
+        doAnimate("luigi-circuit", parseInt(lap3));
+        console.log("Final lap: " + lap3);
+        lapCounter++;
+        $(".js-lap-counter").text(lapCounter);
 
-    setTimeout(function() {
-      $(".circuit-map img").fadeOut(2000);
-    }, parseInt(lap1) + parseInt(lap2) + parseInt(lap3));
+        setTimeout(function() {
+          $(".circuit-map img").fadeOut(2000);
+        }, parseInt(lap3));
+      }, parseInt(lap2));
+    }, parseInt(lap1));
 
   });
 };
+
+function doAnimate(animationName, animationDuration) {
+  var $circuit = $('.js-circuit');
+  $circuit.removeClass(animationName);
+
+  requestAnimationFrame(function () {
+    $circuit.addClass(animationName).css("animation-duration", animationDuration + "ms");
+  });
+}
