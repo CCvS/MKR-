3f91baa455d4b12939532c2a8bbc32f7eba3b4e7
Carl van Straalen <carlvanstraalen@gmail.com>
2017-07-03 00:11:06 +0200

    "Stopwatch" timer works (TODO: Not 100% accurate)

--- js/fill-in-time.js
+++ js/fill-in-time.js
@@ -3,8 +3,7 @@ function enterTime() {
     var lap1 = prompt("Enter " + $(this).data("racer") + "'s" + " 1st lap time in milliseconds", "3000"),
         lap2 = prompt("Enter " + $(this).data("racer") + "'s" + " 2nd lap time in milliseconds", "2000"),
         lap3 = prompt("Enter " + $(this).data("racer") + "'s" + " final lap time in milliseconds", "1000"),
-        lapCounter = 1,
-        timeCounter = 1;
+        lapCounter = 1;
 
     $(".circuit-map").append("<img data-img=\"" + $(this).data("img") + "\" src=\"" + "img/circuits/heads/" + $(this).data("img") + ".png" + "\" class=\"" + "js-circuit" + "\">");
 
@@ -34,18 +33,36 @@ function enterTime() {
       }, parseInt(lap2));
     }, parseInt(lap1));
 
-    function pad(val) { return val > 9 ? val : "0" + val; }
-      var interval = setInterval(function () {
+    function pad(n, width) {
+      n = n + '';
+      return n.length >= width ? n : new Array(width - n.length + 1).join("0") + n;
+    }
 
-        $(".minutes").text(pad(parseInt(timeCounter / 6000, 10)));
-        $(".seconds").text(pad(parseInt(timeCounter / 100, 10) % 60));
-        $(".milliseconds").text(pad(++timeCounter % 100));
-    
-      }, 10);
+    var end = new Date(), // TODO: Not 100% accurate
 
-      setTimeout(function() {
-        clearInterval(interval);
-      }, parseInt(lap1) + parseInt(lap2) + parseInt(lap3));
+        _millisecond = 1,
+        _second = 1000,
+        _minute = 60000,
+        timer;
+
+    function timeCounter() {
+      var now = new Date(),
+          distance = now - end,
+
+          minutes = Math.floor((distance % 3600000) / _minute),
+          seconds = Math.floor((distance % _minute) / _second),
+          milliseconds = Math.floor((distance % _second) / _millisecond);
+
+      document.querySelector('.minutes').innerHTML = pad(minutes, 2);
+      document.querySelector('.seconds').innerHTML = pad(seconds, 2);
+      document.querySelector('.milliseconds').innerHTML = pad(milliseconds, 3);
+    }
+
+    timer = setInterval(timeCounter, 1);
+
+    setTimeout(function() {
+      clearInterval(timer);
+    }, parseInt(lap1) + parseInt(lap2) + parseInt(lap3));
 
   });
 };
